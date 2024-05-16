a542ed479f5d6796af2220c86cbe706ec8279324
Hyuk Kim <rlagur0222@gmail.com>
2024-05-16 22:51:36 +0900

    SpringMVC 공부

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -1,16 +1,16 @@
 package hello.itemservice.web.basic;
 
-
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+
 
 @Controller
 @RequestMapping("/basic/items")
@@ -86,7 +86,7 @@ public class BasicItemController {
     /**
      * ModelAttribute 생략 가능 클래스이름의 첫 글자를 소문자로 하면 모델이름이됨 (Item -> item)
      */
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV3(@ModelAttribute("item") Item item) {
 
         itemRepository.save(item);
@@ -94,6 +94,20 @@ public class BasicItemController {
 //        model.addAttribute("item", item);
 
         return "redirect:/basic/items/"+item.getId();
+    }
+
+
+    /**
+     * redirectAttributes.addAttribute("name", "string") --> 대체할 "name"이 있으면 대체 // 없으면 쿼리파라미터로 넘어감 ?name=string
+     */
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute("item") Item item, RedirectAttributes redirectAttributes) {
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";
     }
 
     /**
